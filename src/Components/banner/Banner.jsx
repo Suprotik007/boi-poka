bf6252dd4c74ce0903371ffc615a17cbe23ff64b
Suprotik007 <csuprotik@gmail.com>
2025-04-20 00:14:08 +0600

    fixed issue

--- src/Components/banner/Banner.jsx
+++ src/Components/banner/Banner.jsx
@@ -8,37 +8,37 @@ const Banner = () => {
 <div className="carousel h-100  mt-15 carousel-center bg-black rounded-box - space-x-5 p-3">
   <div className="carousel-item ">
     <img
-      src="/src/assets/the-origin-of-species-44.jpg"
+      src="the-origin-of-species-44.jpg"
       className="rounded-box" />
   </div>
   <div className="carousel-item">
     <img
-      src="/src/assets/POM.webp"
+      src="POM.webp"
       className="rounded-box" />
   </div>
   <div className="carousel-item">
     <img
-      src="/src/assets/the-alchemist-a-graphic-novel.jpg"
+      src="the-alchemist-a-graphic-novel.jpg"
       className="rounded-box" />
   </div>
   <div className="carousel-item">
     <img
-      src="/src/assets/psychogirl.jpg"
+      src="psychogirl.jpg"
       className="rounded-box" />
   </div>
   <div className="carousel-item">
     <img
-      src="/src/assets/roadless.avif"
+      src="roadless.avif"
       className="rounded-box" />
   </div>
   <div className="carousel-item">
     <img
-      src="/src/assets/3worlds.webp"
+      src="3worlds.webp"
       className="rounded-box" />
   </div>
   <div className="carousel-item">
     <img
-      src="/src/assets/windup.jpg"
+      src="windup.jpg"
       className="rounded-box" />
   </div>
 </div>
